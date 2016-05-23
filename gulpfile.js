de72e8caa7ca2f500af69dcd44e55813b7765324
PythonNut <PythonNut@users.noreply.github.com>
2016-05-23 23:36:41 +0000

    Use pug to generate HTML

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,12 @@
 var gulp  = require('gulp'),
-    gutil = require('gulp-util');
+    gutil = require('gulp-util'),
+    pug   = require('gulp-pug');
 
 gulp.task('default', function () {
   gutil.log("Gulp is running!");
-  // TODO: write something here
+  gulp.src('source/pug/index.pug')
+    .pipe(pug({
+      pretty: !gutil.env.production
+    }))
+    .pipe(gulp.dest('dist'));
 });
