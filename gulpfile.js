910c78b4992ee9aa75d0a1ef1edfad8b9a9ec5c1
PythonNut <PythonNut@users.noreply.github.com>
2016-05-23 23:45:19 +0000

    Use stylus to generate CSS

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,18 @@
 var gulp  = require('gulp'),
     gutil = require('gulp-util'),
-    pug   = require('gulp-pug');
+    pug   = require('gulp-pug'),
+    stylus = require('gulp-stylus');
 
 gulp.task('default', function () {
   gutil.log("Gulp is running!");
   gulp.src('source/pug/index.pug')
     .pipe(pug({
       pretty: !gutil.env.production
+    }))
+    .pipe(gulp.dest('dist'));
+  gulp.src('source/stylus/main.styl')
+    .pipe(stylus({
+      compress: !!gutil.env.production
     }))
     .pipe(gulp.dest('dist'));
 });
