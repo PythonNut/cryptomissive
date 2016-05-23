c4bd2921f5366c1d7bccc655421490f234686129
PythonNut <PythonNut@users.noreply.github.com>
2016-05-23 23:53:44 +0000

    Use typescript to generate JavaScript

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,8 @@
-var gulp  = require('gulp'),
-    gutil = require('gulp-util'),
-    pug   = require('gulp-pug'),
-    stylus = require('gulp-stylus');
+var gulp   = require('gulp'),
+    gutil  = require('gulp-util'),
+    pug    = require('gulp-pug'),
+    stylus = require('gulp-stylus'),
+    ts     = require('gulp-typescript');
 
 gulp.task('default', function () {
   gutil.log("Gulp is running!");
@@ -13,6 +14,12 @@ gulp.task('default', function () {
   gulp.src('source/stylus/main.styl')
     .pipe(stylus({
       compress: !!gutil.env.production
+    }))
+    .pipe(gulp.dest('dist'));
+  gulp.src('source/typescript/main.ts')
+    .pipe(ts({
+      noImplicitAny: true,
+      out: 'main.js'
     }))
     .pipe(gulp.dest('dist'));
 });
