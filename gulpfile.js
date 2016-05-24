760a3e67da8e114fa5e4908ea52acdf6f28d361a
PythonNut <PythonNut@users.noreply.github.com>
2016-05-24 01:23:13 +0000

    Use libsodium.js for crypto

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,14 @@
-var gulp    = require('gulp'),
-    gutil   = require('gulp-util'),
-    pug     = require('gulp-pug'),
-    stylus  = require('gulp-stylus'),
-    ts      = require('gulp-typescript'),
-    postcss = require('gulp-postcss'),
-    apfx    = require('autoprefixer'),
-    inliner = require('gulp-inline-source');
+var gulp     = require('gulp'),
+    merge    = require('merge-stream'),
+    gutil    = require('gulp-util'),
+    concat   = require('gulp-concat'),
+    pug      = require('gulp-pug'),
+    stylus   = require('gulp-stylus'),
+    ts       = require('gulp-typescript'),
+    postcss  = require('gulp-postcss'),
+    apfx     = require('autoprefixer'),
+    inliner  = require('gulp-inline-source'),
+    download = require('gulp-download');
 
 gulp.task('css', function () {
   return gulp.src('source/stylus/main.styl')
@@ -21,12 +24,22 @@ gulp.task('css', function () {
     .pipe(gulp.dest('dist'));
 });
 
-gulp.task('js', function () {
-  return gulp.src('source/typescript/main.ts')
+gulp.task('js-source', function () {
+  var sodium, main;
+  sodium = download('https://raw.githubusercontent.com/jedisct1/libsodium.js/master/dist/browsers/combined/sodium.js')
+    .pipe(gulp.dest('lib'));
+  main = gulp.src('source/typescript/main.ts')
     .pipe(ts({
       noImplicitAny: true,
       out: 'main.js'
     }))
+    .pipe(gulp.dest('dist'));
+  return merge(sodium, main);
+});
+
+gulp.task('js', ['js-source'], function () {
+  return gulp.src(['lib/sodium.js', 'dist/main.js'])
+    .pipe(concat('all.js'))
     .pipe(gulp.dest('dist'));
 });
 
@@ -36,7 +49,7 @@ gulp.task('html', ['css', 'js'], function() {
       pretty: !gutil.env.production
     }))
     .pipe(inliner({
-      rootpath: 'dist',
+      rootpath: '.',
       compress: !!gutil.env.production,
       pretty: true
     }))
