8a4770da7653899e8059a785ae391bc2e413c40d
PythonNut <PythonNut@users.noreply.github.com>
2016-06-03 01:15:29 +0000

    Remove vestigial code in gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,2 @@
-var gulp     = require('gulp'),
-    merge    = require('merge-stream'),
-    gutil    = require('gulp-util'),
-    watch    = require('gulp-watch'),
-    concat   = require('gulp-concat'),
-    pug      = require('gulp-pug'),
-    stylus   = require('gulp-stylus'),
-    ts       = require('gulp-typescript'),
-    postcss  = require('gulp-postcss'),
-    apfx     = require('autoprefixer'),
-    inliner  = require('gulp-inline-source'),
-    download = require('gulp-download');
-
 require('livescript');
 require('./gulpfile.ls');
